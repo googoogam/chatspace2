baddb0419730ffc5501c12901b25f9cc0d59cbd3
googoogam <ken@kennoMacBook.local>
2018-10-24 18:22:36 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   # get 'messages' => 'messages#index'
-  root 'messages#index'
+  root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
     resources :users, only: [:edit, :update]
-end
+    resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+    end
+  end
