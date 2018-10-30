b49b08d72e15783f09c21a73dba4966962ce037c
googoogam <ken@kennoMacBook.local>
2018-10-30 14:07:48 +0900

    timezone"tokyo"追加

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module ChatSpace
     end
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
