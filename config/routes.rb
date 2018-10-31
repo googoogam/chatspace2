e56929a6b777e991f3e3cb274f4b79e505cd3418
googoogam <ken@kennoMacBook.local>
2018-10-31 16:00:21 +0900

    ルーティング調整

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
     resources :users, only: [:index, :edit, :update]
-    resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
+    resources :groups do
+      resources :messages, only: [:index, :create]
 
     end
   end
