ba918750cc2587d33984f7a7e2f51c8c041a3290
googoogam <ken@kennoMacBook.local>
2018-11-04 19:35:18 +0900

    Was

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,12 @@
 # config valid only for current version of Capistrano
 lock '3.11.0'
-
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+set :linked_files, %w{ config/secrets.yml }
 set :application, 'chatspace2'
 set :repo_url,  'git@github.com:gomashiox/chatspace2.git'
 
@@ -21,8 +27,19 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-end
 
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+end
 # config valid for current version and patch releases of Capistrano
 #
 
