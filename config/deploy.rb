2165483abf6612eafa3b58e04cab75207ca37a2a
googoogam <ken@kennoMacBook.local>
2018-11-04 16:52:58 +0900

    Capful

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/ken/.ssh/key_pem']  ※例：/Users/yusukeyamane/.ssh/key_pem.pem
+                  keys: ['/Users/ken/.ssh/ken0609taro.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
