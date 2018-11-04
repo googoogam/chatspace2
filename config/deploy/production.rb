e54fd0ee650964f22a496c1772464b93bdcb043d
googoogam <ken@kennoMacBook.local>
2018-11-04 16:20:49 +0900

    production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-# server-based syntax
+'52.194.125.13', user: 'ec2-user', roles: %w{app db web}# server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
